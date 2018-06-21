d08f0aff64d16f3dac61878eed3a2f109679eeb5
Geir Gåsodden <geir.gasodden@pythonia.no>
2018-06-21 07:41:49 +0200

    Updates engines. Adds more logging (patch)

--- do-jobs.js
+++ do-jobs.js
@@ -1,14 +1,13 @@
-'use strict'
-
 const fs = require('fs')
 const exec = require('child_process').execFile
 const config = require('./config')
 
 function next () {
   const isJsonFile = file => file.indexOf('.json') > -1
-  const jobs = fs.readdirSync(config.JOBS_DIRECTORY_PATH).filter(isJsonFile).length
-  if (jobs > 0) {
-    console.log(jobs + ' jobs left to do')
+  const jobs = fs.readdirSync(config.JOBS_DIRECTORY_PATH).filter(isJsonFile)
+  if (jobs.length > 0) {
+    console.log(jobs.length + ' jobs left to do')
+    console.log(`next job file: ${jobs[0]}`)
     exec('node', ['example.js'], (error, stdout, stderr) => {
       if (error) {
         console.error(error)
--- example.js
+++ example.js
@@ -1,8 +1,7 @@
-'use strict'
-
 const linkChecker = require('./do-job')
 
 linkChecker({}, function (error, message) {
+  console.log('Ready to check link')
   if (error) {
     console.error(error)
   } else {
